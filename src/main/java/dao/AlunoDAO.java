349a81d4b5620228dc053400ebda1df2e984e23a
Vitor0389 <vitor03@gmail.com>
2024-08-18 18:43:07 -0300

    Add remove and list approved

--- src/main/java/dao/AlunoDAO.java
+++ src/main/java/dao/AlunoDAO.java
@@ -3,6 +3,9 @@ package dao;
 import jakarta.persistence.EntityManager;
 import modelo.Aluno;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlunoDAO {
     private EntityManager entityManager;
 
@@ -12,6 +15,16 @@ public class AlunoDAO {
 
     public void cadastrar(Aluno aluno){
         this.entityManager.persist(aluno);
+    }
+    public void excluir(Aluno aluno){
+        this.entityManager.remove(aluno);
+    }
+    public List<Aluno> listarAprovados(){
+        List<Aluno> alunos = new ArrayList<>();
+        String jpql = "SELECT a FROM Aluno a";
+        alunos = this.entityManager.createQuery(jpql, Aluno.class).getResultList();
+        alunos = alunos.stream().filter(aluno -> aluno.calculaMedia() >= 6).toList();
+        return alunos;
     }
 
 }
