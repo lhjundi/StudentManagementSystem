357edb66a11bc0911f9a43120829ade303f8f99f
jundi <lhjundi@outlook.com>
2024-08-23 03:02:24 -0300

    Add try catch blocks

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,81 +2,54 @@ package main;
 
 import dao.AlunoDAO;
 import dao.DaoUtil;
-import jakarta.persistence.EntityManager;
 import modelo.Aluno;
-import util.JPAUtil;
+
 import java.math.BigDecimal;
 import java.util.Scanner;
+
 
 public class Main {
 
     private static final AlunoDAO alunoDAO = DaoUtil.getAlunoDAO();
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        boolean running = true;
-
-        while (running) {
-            System.out.println("------ MENU ------");
-            System.out.println("1. Cadastrar Aluno");
-            System.out.println("2. Excluir Aluno");
-            System.out.println("3. Alterar Aluno");
-            System.out.println("4. Buscar aluno pelo nome");
-            System.out.println("5. Listar alunos (com status de aprovação)");
-            System.out.println("6. Fim do programa");
-            System.out.print("Escolha uma opção: ");
-
-            int option = scanner.nextInt();
-            scanner.nextLine();  // Consome a nova linha após a entrada do número
-
-            switch (option) {
-                case 1:
-                    System.out.println("Cadastrar Aluno");
-                    Aluno novoAluno = criarAluno(scanner);
-                    System.out.println(novoAluno);
-                    alunoDAO.cadastrar(novoAluno);
-                    break;
-                case 2:
-                    System.out.println("Excluir Aluno");
-                    System.out.print("Digite o nome do aluno a ser excluído: ");
-                    String nomeCadastro = scanner.nextLine();
-                    alunoDAO.excluir(nomeCadastro);
-                    break;
-                case 3:
-                    Aluno alunoAntigo;
-                    Aluno alunoNovo;
-                    System.out.println("Alterar Aluno");
-                    System.out.print("Digite o nome do aluno a ser alterado: ");
-                    String nomeAlterar = scanner.nextLine();
-                    alunoAntigo = alunoDAO.buscarAlunoPeloNome(nomeAlterar);
-                    if (alunoAntigo != null) {
-                        alunoNovo = alterarAluno(scanner, alunoAntigo);
-                        alunoDAO.alterarAluno(alunoAntigo, alunoNovo);
-                    } else {
-                        System.out.println("Aluno não encontrado.");
-                    }
-                    break;
-                case 4:
-                    System.out.println("Buscar aluno pelo nome");
-                    System.out.print("Digite o nome do aluno: ");
-                    String nomeBuscar = scanner.nextLine();
-                    System.out.println(alunoDAO.buscarAlunoPeloNome(nomeBuscar));
-                    break;
-                case 5:
-                    System.out.println("Listar alunos (com status de aprovação)");
-                    alunoDAO.listarTodos();
-                    break;
-                case 6:
-                    System.out.println("Fim do programa.");
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Opção inválida.");
-                    break;
+        int opcao;
+        do {
+            menu();
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcao) {
+                case 1 -> cadastrar(scanner);
+                case 2 -> excluir(scanner);
+                case 3 -> editar(scanner);
+                case 4 -> consultar(scanner);
+                case 5 -> exibir();
+                case 6 -> System.out.println("Fim do programa.");
+                default -> System.out.println("Opção inválida.");
             }
-        }
-
+        } while (opcao != 6);
         scanner.close();
+    }
+
+    private static void menu() {
+        System.out.println("------ MENU ------");
+        System.out.println("1. Cadastrar Aluno");
+        System.out.println("2. Excluir Aluno");
+        System.out.println("3. Alterar Aluno");
+        System.out.println("4. Buscar aluno pelo nome");
+        System.out.println("5. Listar alunos (com status de aprovação)");
+        System.out.println("6. Fim do programa");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    private static void cadastrar(Scanner scanner) {
+        System.out.println("\nCadastrar Aluno\n");
+        Aluno novoAluno = criarAluno(scanner);
+        System.out.println(novoAluno);
+        alunoDAO.cadastrar(novoAluno);
+        System.out.println("\nAluno cadastrado com sucesso!\n");
     }
 
     private static Aluno criarAluno(Scanner scanner) {
@@ -100,34 +73,54 @@ public class Main {
 
         scanner.nextLine();
 
-        Aluno aluno = new Aluno();
-        aluno.setNome(nome);
-        aluno.setRa(ra);
-        aluno.setEmail(email);
-        aluno.setNota1(nota1);
-        aluno.setNota2(nota2);
-        aluno.setNota3(nota3);
+        return new Aluno(nome, ra, email, nota1, nota2, nota3);
+    }
 
-        return aluno;
+    private static void excluir(Scanner scanner) {
+        System.out.println("\nExcluir Aluno\n");
+        System.out.print("Digite o nome do aluno a ser excluído: ");
+        String nomeCadastro = scanner.nextLine();
+        try {
+            alunoDAO.excluir(nomeCadastro);
+            System.out.println("\nAluno removido com sucesso!\n");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void editar(Scanner scanner) {
+        Aluno alunoAntigo;
+        Aluno alunoNovo;
+        System.out.println("\nAlterar Aluno\n");
+        System.out.print("Digite o nome do aluno a ser alterado: ");
+        String nomeAlterar = scanner.nextLine();
+        try {
+            alunoAntigo = alunoDAO.buscarAlunoPeloNome(nomeAlterar);
+            alunoNovo = alterarAluno(scanner, alunoAntigo);
+            alunoDAO.alterarAluno(alunoAntigo, alunoNovo);
+            System.out.println("\nAluno alterado com sucesso!\n");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static Aluno alterarAluno(Scanner scanner, Aluno aluno) {
-        System.out.print("Digite o nome do aluno: ");
+        System.out.print("Digite o novo nome do aluno: ");
         String nome = scanner.nextLine();
 
-        System.out.print("Digite o RA do aluno: ");
+        System.out.print("Digite o novo RA do aluno: ");
         String ra = scanner.nextLine();
 
-        System.out.print("Digite o email do aluno: ");
+        System.out.print("Digite o novo email do aluno: ");
         String email = scanner.nextLine();
 
-        System.out.print("Digite a nota 1 do aluno: ");
+        System.out.print("Digite a nova nota 1 do aluno: ");
         BigDecimal nota1 = scanner.nextBigDecimal();
 
-        System.out.print("Digite a nota 2 do aluno: ");
+        System.out.print("Digite a nova nota 2 do aluno: ");
         BigDecimal nota2 = scanner.nextBigDecimal();
 
-        System.out.print("Digite a nota 3 do aluno: ");
+        System.out.print("Digite a nova nota 3 do aluno: ");
         BigDecimal nota3 = scanner.nextBigDecimal();
 
         scanner.nextLine();
@@ -140,5 +133,21 @@ public class Main {
         aluno.setNota3(nota3);
 
         return aluno;
+    }
+
+    private static void consultar(Scanner scanner) {
+        System.out.println("\nBuscar aluno pelo nome\n");
+        System.out.print("Digite o nome do aluno: ");
+        try {
+            String nomeBuscar = scanner.nextLine();
+            System.out.println(alunoDAO.buscarAlunoPeloNome(nomeBuscar));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void exibir() {
+        System.out.println("\nExibindo todos os alunos:\n");
+        alunoDAO.listarTodos();
     }
 }
